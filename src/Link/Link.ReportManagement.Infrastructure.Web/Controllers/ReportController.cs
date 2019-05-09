6ecdf90cfeb807c0ba239f221d7e884fdcdb7a1f
Denis Yerusalimtsev <denis.yerusalimtsev@gmail.com>
2019-05-09 12:08:00 +0300

    Integrate GenerateReport feature to ReportController

---
+++ src/Link/Link.ReportManagement.Infrastructure.Web/Controllers/ReportController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Link.ReportManagement.Application;
+using Link.ReportManagement.Application.Features.GenerateReport;
+using Link.ReportManagement.Domain.Model.Entities;
+using Link.ReportManagement.Infrastructure.Web.Models;
+
+namespace Link.ReportManagement.Infrastructure.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+        private readonly LinkApplication _app;
+
+        public ReportController(LinkApplication app)
+        {
+            _app = app;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GenerateReport([FromBody] ReportParameters parameters)
+        {
+            GenerateReportQuery query = new GenerateReportQuery(parameters);
+            GenerateReportQueryResult result = await _app.RunQuery(query);
+
+            return Ok(new GenerateReportDto()
+            {
+                Report = result.Report
+            });
+        }
+    }
+}
---
+++ src/Link/Link.ReportManagement.Infrastructure.Web/Models/GenerateReportDto.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Link.ReportManagement.Infrastructure.Web.Models
+{
+    public class GenerateReportDto
+    {
+        public MemoryStream Report { get; set; }
+    }
+}
