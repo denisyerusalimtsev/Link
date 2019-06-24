557e5ae1a9ef282f6651ac408f863ab0d33a5a04
Denis Yerusalimtsev <denis.yerusalimtsev@gmail.com>
2019-06-24 19:35:31 +0300

    Add schema for implementing translation

--- src/Link/Link.Frontend/Link-Frontend/src/app/app.module.ts
+++ src/Link/Link.Frontend/Link-Frontend/src/app/app.module.ts
@@ -3,8 +3,11 @@ import { NgModule } from '@angular/core';
 import { MaterialModule } from './material/material.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
 import { AppComponent } from './app.component';
 import { ListEventComponent } from './events/list-event/list-event.component';
 import { ListExpertComponent } from './experts/list-expert/list-expert.component';
@@ -22,6 +25,9 @@ import { MapEventComponent } from './map/map-event/map-event.component';
 import { StateService } from './services/state.service';
 import { DialogAssignExpertsComponent } from './map/dialog-assign-experts/dialog-assign-experts.component';
 
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
@@ -44,7 +50,14 @@ import { DialogAssignExpertsComponent } from './map/dialog-assign-experts/dialog
     ReactiveFormsModule,
     FormsModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   entryComponents: [
     DialogUserComponent,
